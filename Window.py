cfb1b96050e9dece0f0ea433d7e619c1e78a38ac
AceAtomz <93909209+AceAtomz@users.noreply.github.com>
2022-08-29 16:56:08 +0200

    comments

--- Window.py
+++ Window.py
@@ -1,14 +1,12 @@
-import sys
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
-from PyQt5 import QtCore, QtGui, QtWidgets
 
-class Window(QWidget):
+class Window(QWidget):  #Base Class Window
     def __init__(self):
         super ().__init__()
 
-        self.title = "Window"
+        self.title = "Window" #define window attributes
         self.left = 800
         self.top = 300
         self.width = 300
@@ -16,17 +14,17 @@ class Window(QWidget):
 
         self.InitUI()
 
-    def InitUI(self) :
+    def InitUI(self) :  #set window attributes
         self.setWindowTitle (self. title)
         self.setGeometry (self.left, self.top, self. width, self.height)
         self.vbox = QVBoxLayout()
         self.setLayout (self.vbox)
 
-class MainWindow(Window):
+class MainWindow(Window): #Subclass MainWindow
     def __init__(self):
         super().__init__()
 
-        self.title = "Give-a-Little"
+        self.title = "Give-a-Little" #define window attributes
         self.left = 250
         self.top = 250
         self.width = 1000
@@ -36,9 +34,9 @@ class MainWindow(Window):
         self.addLoginButtons()
         self.show()
 
-    def addLoginButtons(self):
+    def addLoginButtons(self):               #Add login/register buttons
         self.hLayoutWidget = QWidget(self)
-        self.hLayoutWidget.setGeometry(QtCore.QRect(600, 0, 400, 50))
+        self.hLayoutWidget.setGeometry(QRect(600, 0, 400, 50))
         self.hLayoutWidget.setObjectName("hLayoutWidget")
         self.hLayout = QHBoxLayout(self.hLayoutWidget)
         self.hLayout.setContentsMargins(20, 0, 20, 0)
@@ -46,22 +44,22 @@ class MainWindow(Window):
         self.hLayout.setObjectName("hLayout")
         self.vbox.addWidget(self.hLayoutWidget)
 
-        self.btnLogin = QPushButton(self.hLayoutWidget)
+        self.btnLogin = QPushButton(self.hLayoutWidget) #Login Button
         self.btnLogin.setObjectName("btnLogin")
         self.btnLogin.setText("Login")
         self.btnLogin.clicked.connect(self.btnLogin_clicked)
         self.hLayout.addWidget(self.btnLogin)
 
-        self.btnRegister = QtWidgets.QPushButton(self.hLayoutWidget)
+        self.btnRegister = QPushButton(self.hLayoutWidget) #Register button
         self.btnRegister.setObjectName("btnRegister")
         self.btnRegister.setText("Register")
         self.btnRegister.clicked.connect(self.btnRegister_clicked)
         self.hLayout.addWidget(self.btnRegister)
 
-    def btnLogin_clicked(self):
+    def btnLogin_clicked(self): #Open new Window when login button pressed
         self.mydialog = Window()
         self.mydialog.show()
 
-    def btnRegister_clicked(self):
+    def btnRegister_clicked(self): #Open new Window when register button pressed
         self.mydialog = Window()
         self.mydialog.show()
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from Window import *
 
 def main():
    app = QApplication(sys.argv)
-   ex = MainWindow()
+   ex = MainWindow()                    #create MainWindow object
    sys.exit(app.exec_())
 
 if __name__ == "__main__":
