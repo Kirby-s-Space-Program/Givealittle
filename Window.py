7fc2f6c3309a810f251b3ba86d31d409ec28c3df
AceAtomz <93909209+AceAtomz@users.noreply.github.com>
2022-08-29 18:23:23 +0200

    login window subclass

--- Window.py
+++ Window.py
@@ -4,64 +4,69 @@ from PyQt5.QtWidgets import *
 from configurations import *
 
 class Window(QWidget):  #Base Class Window
-    def __init__(self):
-        super ().__init__()
+   def __init__(self):
+      super ().__init__()
 
-        self.title = "Window" #define window attributes
-        self.left = 800
-        self.top = 300
-        self.width = 300
-        self.height = 250
+      self.title = "Window" #define window attributes
+      self.left = LEFT_SUB
+      self.top = TOP_SUB
+      self.width = WIDTH_SUB
+      self.height = HEIGHT_SUB
 
-        self.InitUI()
+      self.InitUI()
 
-    def InitUI(self) :  #set window attributes
-        self.setWindowTitle (self.title)
-        self.setGeometry (self.left, self.top, self. width, self.height)
-        self.setWindowIcon(QIcon("logo.png"))
-        self.vbox = QVBoxLayout()
-        self.setLayout (self.vbox)
+   def InitUI(self) :  #set window attributes
+      self.setWindowTitle(self.title)
+      self.setGeometry(self.left, self.top, self. width, self.height)
+      self.setWindowIcon(QIcon("logo.png"))
 
 class MainWindow(Window): #Subclass MainWindow
-    def __init__(self):
-        super().__init__()
+   def __init__(self):
+      super().__init__()
 
-        self.title = "Give-a-Little" #define window attributes
-        self.left = 0
-        self.top = 0
-        self.width = SIZE_WIDTH_MAIN
-        self.height = SIZE_HEIGHT_MAIN
+      self.title = "Give-a-Little" #define window attributes
+      self.left = 0
+      self.top = 0
+      self.width = WIDTH_MAIN
+      self.height = HEIGHT_MAIN
 
-        self.InitUI()
-        self.addLoginButtons()
-        self.showMaximized()
-        self.show()
+      self.InitUI()
+      self.addLoginButtons()
+      self.showMaximized()
+      self.show()
 
-    def addLoginButtons(self):               #Add login/register buttons
-        self.hLayoutWidget = QWidget(self)
-        self.hLayoutWidget.setGeometry(QRect(HLAYOUT_LEFT, HLAYOUT_TOP, HLAYOUT_WIDTH, HLAYOUT_HEIGHT))
-        self.hLayoutWidget.setObjectName("hLayoutWidget")
-        self.hLayout = QHBoxLayout(self.hLayoutWidget)
-        self.hLayout.setSpacing(HLAYOUT_SPACING)
-        self.hLayout.setObjectName("hLayout")
-        self.vbox.addWidget(self.hLayoutWidget)
+   def addLoginButtons(self):               #Add login/register buttons
+      self.hLayoutWidget = QWidget(self)
+      self.hLayoutWidget.setGeometry(QRect(HLAYOUT_LEFT, HLAYOUT_TOP, HLAYOUT_WIDTH, HLAYOUT_HEIGHT))
+      self.hLayoutWidget.setObjectName("hLayoutWidget")
+      self.hLayout = QHBoxLayout(self.hLayoutWidget)
+      self.hLayout.setSpacing(HLAYOUT_SPACING)
+      self.hLayout.setObjectName("hLayout")
 
-        self.btnLogin = QPushButton(self.hLayoutWidget) #Login Button
-        self.btnLogin.setObjectName("btnLogin")
-        self.btnLogin.setText("Login")
-        self.btnLogin.clicked.connect(self.btnLogin_clicked)
-        self.hLayout.addWidget(self.btnLogin)
+      self.btnLogin = QPushButton(self.hLayoutWidget) #Login Button
+      self.btnLogin.setObjectName("btnLogin")
+      self.btnLogin.setText("Login")
+      self.btnLogin.clicked.connect(self.btnLogin_clicked)
+      self.hLayout.addWidget(self.btnLogin)
 
-        self.btnRegister = QPushButton(self.hLayoutWidget) #Register button
-        self.btnRegister.setObjectName("btnRegister")
-        self.btnRegister.setText("Register")
-        self.btnRegister.clicked.connect(self.btnRegister_clicked)
-        self.hLayout.addWidget(self.btnRegister)
+      self.btnRegister = QPushButton(self.hLayoutWidget) #Register button
+      self.btnRegister.setObjectName("btnRegister")
+      self.btnRegister.setText("Register")
+      self.btnRegister.clicked.connect(self.btnRegister_clicked)
+      self.hLayout.addWidget(self.btnRegister)
 
-    def btnLogin_clicked(self): #Open new Window when login button pressed
-        self.mydialog = Window()
-        self.mydialog.show()
+   def btnLogin_clicked(self): #Open new Window when login button pressed
+      self.mydialog = LoginWindow()
+      self.mydialog.show()
 
-    def btnRegister_clicked(self): #Open new Window when register button pressed
-        self.mydialog = Window()
-        self.mydialog.show()
+   def btnRegister_clicked(self): #Open new Window when register button pressed
+      self.mydialog = Window()
+      self.mydialog.show()
+
+class LoginWindow(Window): #Subclass LoginWindow
+   def __init__(self):
+      super().__init__()
+
+      self.title = "Login"
+
+      self.InitUI()
--- configurations.py
+++ configurations.py
@@ -6,11 +6,16 @@ app = QtWidgets.QApplication(sys.argv)
 screen = app.primaryScreen()
 
 #Sizes
-SIZE_WIDTH_MAIN = screen.size().width()
-SIZE_HEIGHT_MAIN = screen.size().height()
+WIDTH_MAIN = screen.size().width()
+HEIGHT_MAIN = screen.size().height()
 
-HLAYOUT_HEIGHT = int(SIZE_HEIGHT_MAIN/20)
-HLAYOUT_WIDTH = int(SIZE_WIDTH_MAIN/5)
-HLAYOUT_LEFT = SIZE_WIDTH_MAIN-HLAYOUT_WIDTH-100
+HLAYOUT_HEIGHT = int(HEIGHT_MAIN/20)
+HLAYOUT_WIDTH = int(WIDTH_MAIN/5)
+HLAYOUT_LEFT = WIDTH_MAIN-HLAYOUT_WIDTH-100
 HLAYOUT_TOP = 0
 HLAYOUT_SPACING = 20
+
+WIDTH_SUB = int(WIDTH_MAIN/4)
+HEIGHT_SUB = int(HEIGHT_MAIN/2.5)
+LEFT_SUB = int(WIDTH_MAIN/2 - WIDTH_SUB/2)
+TOP_SUB = int(HEIGHT_MAIN/4)
