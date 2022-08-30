8ae6b1f4f58ba0d350f8125d8968c139c0bbb827
AceAtomz <93909209+AceAtomz@users.noreply.github.com>
2022-08-30 22:28:56 +0200

    comments

--- encrypter.py
+++ encrypter.py
@@ -1,8 +1,8 @@
 import hashlib
 import uuid
 
-def encrypt(password, *args, **kwargs):
-    salt = kwargs.get('salt', uuid.uuid4().hex)
+def encrypt(password, **kwargs):
+    salt = kwargs.get('salt', uuid.uuid4().hex) #if no salt given, creates new one
     salt = salt.encode()
-    snhpassword = hashlib.sha512(password.encode() + salt).hexdigest()
+    snhpassword = hashlib.sha512(password.encode() + salt).hexdigest()  #salts and hashes password
     return snhpassword
