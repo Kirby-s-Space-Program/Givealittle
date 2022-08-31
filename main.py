bca25bc21a03ec30c46eebc99379d32f841674bf
AceAtomz <93909209+AceAtomz@users.noreply.github.com>
2022-08-31 16:40:02 +0200

    unit testing

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from Window import *
 def main():
    app = QApplication(sys.argv)
    ex = MainWindow()                    #create MainWindow object
+   ex.show()
    sys.exit(app.exec_())
 
 if __name__ == "__main__":
